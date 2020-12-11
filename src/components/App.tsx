a36f78aefe8e230fad68c83de110224477410867
Soin Na <carrotcold@gmail.com>
2020-12-12 01:59:43 +0900

    :wrench: react-redux configuration

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pengsoo Run</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { rootReducer } from './features';
+
+import App from './components/App';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
